e201a9bfd6db21c6170b582761497d3b8daa1684
Eliran Malka <eliranmal@gmail.com>
2017-11-16 02:16:30 +0200

    Create wallpaper-all-spaces.sh

---
+++ wallpaper-all-spaces.sh
@@ -0,0 +1,8 @@
+function set_wallpaper {
+
+	local IMAGE="$1"
+
+	sqlite3 ~/Library/Application\ Support/Dock/desktoppicture.db "UPDATE data SET value = '$IMAGE'" && killall Dock
+}
+
+set_wallpaper
